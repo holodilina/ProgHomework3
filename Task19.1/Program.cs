b5b315f9f9f4f2b751eebc676ba5195775dedc75
Natalya <holodilina.n@gmail.com>
2022-09-13 16:18:29 +0300

    Add folder Task21

---
+++ Task19.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+using System;
+public class Exercise38
+{
+    public static void Main()
+    {
+        int num, r, sum=0, t;
+
+        Console.Write("\n\n");
+        
+    }
+}
