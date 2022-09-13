0588b23b28c5627a7fc0b1a58fb3e0ae47042a8c
Natalya <holodilina.n@gmail.com>
2022-09-13 18:24:34 +0300

    внесение изменений в задачу 21

---
+++ Task21.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("\n\n");
+
+double x1 = 7;
+double y1 = -5;
+double z1 = 0;
+
+double x2 = 1;
+double y2 = -1;
+double z2 = 9;
+  
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
+
+Console.WriteLine($"Расстояние равно: {d}");
+
+Console.Write("\n\n");
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,29 +1,33 @@
 ﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+Console.Write("\n\n"); 
+
+Console.Write("ВВЕДИТЕ КООРДИНАТЫ ДВУХ ТОЧЕК: ");
+
 Console.Write("\n\n");
-Console.WriteLine("ВВЕДИТЕ КООРДИНАТЫ ДВУХ ТОЧЕК: ");
 
-{
-    {
-    int a;
-a = Convert.ToInt32(Console.ReadLine());
+Console.Write("x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
 
-int b;
-b = Convert.ToInt32(Console.ReadLine());
-    }
+Console.Write("y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
 
-{
-    void distance(float x1, float y1, float z1, float x2, float y2, float z3)
-    {
-       double d = Math.Pow((Math.Pow(x2 - x1, 2) + 
-                  Math.Pow(Math.Pow(y2 - y1, 2) + 
-                  Math.Pow(Math.Pow(z2 - z1, 2) * 
-                  1.0, 0.5);
+Console.Write("z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Расстояние равно" + d);
-        return;
-    }
-    
-}
+Console.Write("x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
 
-}
+Console.Write("y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+  
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
+
+Console.Write("\n\n");
+
+Console.WriteLine($"РАСТОЯНИЕ РАВНО: {d}");
+
+Console.Write("\n\n");
