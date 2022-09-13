a24b61997eb5c7e03a2eceb8bdcf951975044a19
Natalya <holodilina.n@gmail.com>
2022-09-13 17:16:10 +0300

    изменения в таск21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,4 +1,29 @@
 ﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("\n\n");
 Console.WriteLine("ВВЕДИТЕ КООРДИНАТЫ ДВУХ ТОЧЕК: ");
 
+{
+    {
+    int a;
+a = Convert.ToInt32(Console.ReadLine());
 
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+    }
+
+{
+    void distance(float x1, float y1, float z1, float x2, float y2, float z3)
+    {
+       double d = Math.Pow((Math.Pow(x2 - x1, 2) + 
+                  Math.Pow(Math.Pow(y2 - y1, 2) + 
+                  Math.Pow(Math.Pow(z2 - z1, 2) * 
+                  1.0, 0.5);
+
+        Console.WriteLine("Расстояние равно" + d);
+        return;
+    }
+    
+}
+
+}
