5c78acd4bd0155e3da199a6585a51bcb6555b1af
Natalya <holodilina.n@gmail.com>
2022-09-13 16:50:05 +0300

    внесение изменений в 19.1

--- Task19.1/Program.cs
+++ Task19.1/Program.cs
@@ -1,13 +1,25 @@
 ﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 using System;
-public class Exercise38
 {
-    public static void Main()
+
     {
         int num, r, sum=0, t;
 
         Console.Write("\n\n");
+        Console.Write("ВВЕДИТЕ ПЯТИЗНАЧНОЕ ЧИСЛО: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        for(t=num;num!=0;num=num/10)
+        {
+            r=num % 10;
+            sum=sum*10+r;
+        }
+        if(t==sum)
+            Console.Write("{0} -> это палиндром",t);
+        else
+            Console.Write("{0} -> это не палиндром",t);
         
+       
     }
+     Console.Write("\n\n");
 }
