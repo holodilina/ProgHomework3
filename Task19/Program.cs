8d8576d9a925a07f14c78e77f09c41b679044b96
Natalya <holodilina.n@gmail.com>
2022-09-13 16:03:16 +0300

    задание 19 фиксация

---
+++ Task19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("ВВЕДИТЕ ПЯТИЗНАЧНОЕ ЧИСЛО: ");
+
+var input = Console.ReadLine();
+int num;
+if(string.IsNullOrWhiteSpace(input)
+||input.Length!=5
+||!int.TryParse(input, out num))
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+var inverse=string.Empty;
+
+for(var i = input.Length-1; i>=0; i--)
+{
+    inverse+=input[i];
+}    
+
+if (string.Compare(inverse, input) == 0)
+{
+    Console.WriteLine(" -> палиндромом");
+}
+else{
+        Console.WriteLine(" -> НЕ палиндромом");      
+}
+
+
+
