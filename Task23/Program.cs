7d5dab45d15217743b69db45234120a08f1a28bb
Natalya <holodilina.n@gmail.com>
2022-09-13 18:45:51 +0300

    Add folder Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("ЗАДАЙТЕ ЧИСЛО: ");
+
+var input = Console.ReadLine();
+int num;
+if(string.IsNullOrWhiteSpace(input) 
+||!int.TryParse(input, out num))
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+for(var i=1;i<=num;i++)
+{
+    var pow = Math.Pow(i,3);
+    Console.WriteLine($"{pow}");   
+} 
